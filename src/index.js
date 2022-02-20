b8e1b1d9324fabf9215a5f601c08f8b450bca181
Naqash <naqashmuhammad672@gmail.com>
2022-02-20 09:57:27 -0700

    web

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+
+  <>
+
+    <h1>my fevrit movies</h1>
+    <p>list of 5 best movies</p>
+    <ol>
+      <li>dill waly</li>
+      <li>dill waly</li>
+      <li>dill waly</li>
+      <li>dill waly</li>
+    </ol>
+  </>
+
+  , document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log resu
